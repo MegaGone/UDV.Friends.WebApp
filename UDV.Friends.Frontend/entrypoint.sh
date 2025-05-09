332e96d052d1fc86ff32e33f9b6709becbf65660
MegaGone <jimmymartinez016@gmail.com>
2025-05-09 12:40:45 -0600

    [FIX] - Copy correct build files into angular container

--- UDV.Friends.Frontend/entrypoint.sh
+++ UDV.Friends.Frontend/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-# Reemplazar variables de entorno en los archivos JavaScript
-echo "Configurando variables de entorno:"
-echo "FRIEND_CHANNEL=${FRIEND_CHANNEL}"
-echo "BASE_URL=${BASE_URL}"
+FRIEND_CHANNEL_ESCAPED=$(printf '%s\n' "$FRIEND_CHANNEL" | sed -e 's/[\/&]/\\&/g')
+BASE_URL_ESCAPED=$(printf '%s\n' "$BASE_URL" | sed -e 's/[\/&]/\\&/g')
 
-for file in /usr/share/nginx/html/*.js; do
-  sed -i "s|FRIEND_CHANNEL|${FRIEND_CHANNEL}|g" $file
-  sed -i "s|BASE_URL|${BASE_URL}|g" $file
-done
+find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' -o -name '*.html' \) -exec sed -i "s|FRIEND_CHANNEL_PLACEHOLDER|${FRIEND_CHANNEL_ESCAPED}|g" {} \;
+find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' -o -name '*.html' \) -exec sed -i "s|BASE_URL_PLACEHOLDER|${BASE_URL_ESCAPED}|g" {} \;
 
-# Iniciar nginx
 exec "$@"
